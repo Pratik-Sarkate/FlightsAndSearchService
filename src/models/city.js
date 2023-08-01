c67d51ea90da28f85fb3db74074f7e5f7f13d4e2
Pratik-Sarkate <pratiksarkate2242@gmail.com>
2023-08-02 02:32:32 +0530

    Added unique constraint and started cityrepository

--- src/models/city.js
+++ src/models/city.js
@@ -14,7 +14,9 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   City.init({
-    name: DataTypes.STRING
+    name: DataTypes.STRING,
+    allowNULL: false,
+    unique: true
   }, {
     sequelize,
     modelName: 'City',
