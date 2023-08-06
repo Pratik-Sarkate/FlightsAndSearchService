308e81caba046c170f3f139b137cdf0a5f4034cf
Pratik-Sarkate <pratiksarkate2242@gmail.com>
2023-08-06 17:06:51 +0530

    Added more routers for city api and updated a minor migration file error

--- src/models/city.js
+++ src/models/city.js
@@ -14,9 +14,11 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   City.init({
-    name: DataTypes.STRING,
-    allowNULL: false,
-    unique: true
+    name: {
+      type: DataTypes.STRING,
+      allowNULL: false,
+      unique: true
+    },
   }, {
     sequelize,
     modelName: 'City',
