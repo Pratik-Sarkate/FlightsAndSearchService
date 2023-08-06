d0751c4a5bb80affe391ce91bf15947c50548d5f
Pratik-Sarkate <pratiksarkate2242@gmail.com>
2023-08-06 12:24:48 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -6,18 +6,45 @@ class CityRepository{
             const city = await City.create({name});
             return city;
         } catch (error) {
+            console.log("Something went wrong in repository layer");
             throw {error};
         }
     }
 
-    async deleteCity(cityid){
+    async deleteCity(cityId){
         try {
             await City.destroy({
                 where:{
-                    id: cityid
+                    id: cityId
                 }
             })
         } catch (error) {
+            console.log("Something went wrong in repository layer");
+            throw {error};
+        }
+    }
+
+    async updateCity(cityId, data){
+        try {
+            const city = await City.update(data,{
+                where: {
+                    id: cityId
+                }
+            });
+            return city;
+            
+        } catch (error) {
+            console.log("Something went wrong in repository layer");
+            throw {error};
+        }
+    }
+
+    async getCity(cityId){
+        try {
+            const city = await City.findByPk(cityId);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in repository layer");
             throw {error};
         }
     }
